60fe9133dd82eb1ad385d87a9982c209540b6dae
Akexorcist <akexorcist@gmail.com>
2021-03-11 03:35:10 +0700

    Add adapter

---
+++ app/src/main/java/com/akexorcist/lovelyrecyclerview/adapter/OrderDetailAdapter.kt
@@ -0,0 +1,15 @@
+package com.akexorcist.lovelyrecyclerview.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class OrderDetailAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return /* ... */
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int = 0
+}
