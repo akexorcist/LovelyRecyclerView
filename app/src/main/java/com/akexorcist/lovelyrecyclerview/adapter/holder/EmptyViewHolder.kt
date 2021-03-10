483735ee5f2fcc275316ec5f24a94bdc27c17c0c
Akexorcist <akexorcist@gmail.com>
2021-03-11 03:33:57 +0700

    Update master

--- app/src/main/java/com/akexorcist/lovelyrecyclerview/adapter/holder/EmptyViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.akexorcist.lovelyrecyclerview.adapter.holder
-
-import androidx.recyclerview.widget.RecyclerView
-import com.akexorcist.lovelyrecyclerview.databinding.ViewEmptyBinding
-
-class EmptyViewHolder(binding: ViewEmptyBinding) : RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/akexorcist/lovelyrecyclerview/adapter/holder/NoticeViewHolder.kt
+++
@@ -1,6 +0,0 @@
-package com.akexorcist.lovelyrecyclerview.adapter.holder
-
-import androidx.recyclerview.widget.RecyclerView
-import com.akexorcist.lovelyrecyclerview.databinding.ViewNoticeBinding
-
-class NoticeViewHolder(binding: ViewNoticeBinding) : RecyclerView.ViewHolder(binding.root)
